572e84ce7db91300f1c672ac41a2148ceee30d5f
Roberto Del Franco <rdel-fra@c3r5p5.42sp.org.br>
2024-11-29 18:00:04 -0300

    norminette

--- libft/ft_lstsize_bonus.c
+++ libft/ft_lstsize_bonus.c
@@ -6,7 +6,7 @@
 /*   By: rdel-fra <rdel-fra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 14:30:01 by rdel-fra          #+#    #+#             */
-/*   Updated: 2024/11/29 17:21:08 by rdel-fra         ###   ########.fr       */
+/*   Updated: 2024/11/29 17:56:38 by rdel-fra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int	ft_lstsize(t_list *lst)
 {
 	t_list	*ptr;
-	int				i;
+	int		i;
 
 	i = 0;
 	ptr = lst;
